45cbc0df4634511a0665daf155011bfe173a73b8
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-09-18 22:55:05 +0530

    last commit

--- pages/Thumbnail.js
+++ pages/Thumbnail.js
@@ -5,8 +5,9 @@ function Thumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div>
+    <div className=" group p-4 transition ease-in delay-200  hover:scale-110 hover: duration-200">
       <Image
+        className="rounded-sm"
         layout="responsive"
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
@@ -15,8 +16,11 @@ function Thumbnail({ result }) {
         height={1080}
         width={1920}
       />
-      <div className=" p-2">
-        <h3> {result.title}</h3>
+      <div className=" p-2 group cursor-pointer  ">
+        <h3 className="text-2xl ease-in hover:scale-110 hover: duration-200 ">
+          {" "}
+          {result.title}
+        </h3>
         <p className="truncate max-w-md">{result.overview}</p>
       </div>
     </div>
